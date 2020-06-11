a3c9d0964f06a0e404e0feb655e370fcdf587811
Andi Fajar Nugraha <andifajarlah@gmail.com>
2020-06-11 09:13:33 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,21 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$router->get('/', function (){
+    return "API SIMAK DINAS PENDIDIKAN PROVINSI NTB";
+});
+
+Route::prefix('review')->group(function () {
+    Route::get('/', 'ReviewController@index');
+    Route::get('/form', 'ReviewController@review');
+    Route::put('/review', 'ReviewController@do_review');
+    Route::put('/selesai', 'ReviewController@selesai');
+});
+
+Route::prefix('disposisi')->group(function () {
+    Route::get('/', 'DisposisiController@index');
+    Route::get('/form', 'DisposisiController@disposisi');
+    Route::put('/disposisi', 'DisposisiController@do_disposisi');
+    Route::put('/selesai', 'DisposisiController@selesai');
+});
